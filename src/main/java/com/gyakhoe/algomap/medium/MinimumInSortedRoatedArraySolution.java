b7d854b2051f088b132fd8e85163f5b6d4e4846f
Gyague Sonam <gyakhoe@gmail.com>
2024-09-12 12:06:09 -0400

    feat: add solution for leetcode question 154 for finding minimum in rotated sorted array

---
+++ src/main/java/com/gyakhoe/algomap/medium/MinimumInSortedRoatedArraySolution.java
@@ -0,0 +1,21 @@
+package com.gyakhoe.algomap.medium;
+
+public class MinimumInSortedRoatedArraySolution {
+
+    public int findMinimum(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right){
+            int middle = (left + right ) / 2;
+            if(nums[middle] > nums[right]) {
+                left = middle + 1;
+            }else {
+                right = middle;
+            }
+        }
+
+        return nums[left];
+    }
+}
